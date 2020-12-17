ba2fb37e1bd37a59e0f9183464011a0e5ca296ba
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 23:08:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,26 +3,25 @@ function fetchBooks(){
 }
 function renderBooks(books){
   let main = document.querySelector('main')
-  let data = document.getElementById('specific-data')
 
   books.forEach(book => {
     let booksName = document.createElement('h3')
     booksName.innerHTML = book.name
     main.appendChild(booksName)
-
-    let characterNb = document.createElement('h3')
-    characterNb.innerHTML = `the 1031st character is ${fetchCharacter(book)}`
-    data.appendChild(characterNb)
   })
   let thirdBook = document.createElement('h3')
   thirdBook.innerHTML = `the Third book from this serie is ${books[2].name}`
   data.appendChild(thirdBook)
 }
+
 function fetchCharacter(){
   return fetch('https://anapioficeandfire.com/api/characters').then(resp => resp.json()).then(data => renderCharactersNumb(data))
 }
 function renderCharactersNumb(book){
-
+  let data = document.getElementById('specific-data')
+  let characterNb = document.createElement('h3')
+  characterNb.innerHTML = `the 1031st character is ${fetchCharacter(book)}`
+  data.appendChild(characterNb)
 }
 document.addEventListener('DOMContentLoaded',() => {
   fetchBooks()
