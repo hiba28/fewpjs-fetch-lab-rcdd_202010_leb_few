91f8c344624891b21708b461318f5e0d39ff55e4
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 22:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function renderBooks(books){
   })
   let data = document.getElementById('specific-data')
   let h3 = document.createElement('h3')
-  h3.innerHTML = `the Third book from this serie is ${book[2]}`
+  h3.innerHTML = `the Third book from this serie is ${books[2]}`
   data.appendChild(h3)
 }
 document.addEventListener('DOMContentLoaded',() => {
