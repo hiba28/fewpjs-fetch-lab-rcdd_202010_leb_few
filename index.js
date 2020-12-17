2d3fbeebbc939ba07362f23aa2ca6ecb30044eb3
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 23:07:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ function renderBooks(books){
     main.appendChild(booksName)
 
     let characterNb = document.createElement('h3')
-    characterNb.innerHTML = `the 1031st character is ${renderCharactersNumb(book)}`
+    characterNb.innerHTML = `the 1031st character is ${fetchCharacter(book)}`
     data.appendChild(characterNb)
   })
   let thirdBook = document.createElement('h3')
   thirdBook.innerHTML = `the Third book from this serie is ${books[2].name}`
   data.appendChild(thirdBook)
 }
+function fetchCharacter(){
+  return fetch('https://anapioficeandfire.com/api/characters').then(resp => resp.json()).then(data => renderCharactersNumb(data))
+}
 function renderCharactersNumb(book){
-
+  
 }
 document.addEventListener('DOMContentLoaded',() => {
   fetchBooks()
